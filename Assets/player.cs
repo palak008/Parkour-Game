13302ec428a197f13ebe8a4b3fc24a6671b59cf6
Palak Bansal <palakbl96@gmail.com>
2020-07-20 19:03:07 +0530

    adding files

---
+++ Assets/player.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public Animator anim;
+	public Rigidbody rbody;
+	private float inputH;
+	private float inputV;
+	public AudioSource audioSource;
+	public AudioClip Journey;
+	private bool run;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent< Animator>();
+		rbody=GetComponent<Rigidbody>();
+		audioSource.clip = Journey;
+		audioSource.Play();
+		run = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown("1"))
+		{
+			anim.Play ("WAIT01", -1, 0f);
+	    }
+		if(Input.GetKeyDown("2"))
+		{
+			anim.Play ("WAIT02", -1, 0f);
+		}
+		if(Input.GetKeyDown("3"))
+		{
+			anim.Play ("WAIT03", -1, 0f);
+		}if(Input.GetKeyDown("4"))
+		{
+			anim.Play ("WAIT04", -1, 0f);
+		}
+		if(Input.GetMouseButtonDown(0))//gets damaged on left click
+		{
+			int n = Random.Range (0, 2);
+			if (n == 0) {
+				anim.Play ("DAMAGED00", -1, 0f);
+
+			} else {
+				anim.Play ("DAMAGED01", -1, 0f);
+			}
+
+		}
+		if (Input.GetKey(KeyCode.LeftShift)) {//runs on pressing left shift key
+			run = true;
+		} else {
+			run = false;
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			anim.SetBool("jump",true);
+		} else {
+			anim.SetBool("jump" ,false);
+		}
+		inputH = Input.GetAxis ("Horizontal");
+		inputV = Input.GetAxis ("Vertical");
+		anim.SetFloat ("inputH", inputH);
+		anim.SetFloat ("inputV", inputV);
+		anim.SetBool ("run",run);
+
+		float moveX = inputH * 20f * Time.deltaTime;
+		float moveZ = inputV * 20f * Time.deltaTime;
+		if(moveZ <= 0f)
+		{
+			moveX=0f;
+		}
+		else if(run){
+			moveX *= 3f;
+			moveZ *= 3f;
+		}
+		rbody.velocity = new Vector3 (moveX, 0f, moveZ);
+
+
+}
+}
